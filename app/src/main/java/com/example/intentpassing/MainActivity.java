b9e05a0e2d367f31cd85eaa4b742435ef9821cff
shivank <s.b.dayalpur@gamil.com>
2023-07-08 09:54:35 +0530

    intent passing android project

---
+++ app/src/main/java/com/example/intentpassing/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.intentpassing;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnnext;
+        btnnext = findViewById(R.id.btnnext);
+        btnnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iNext;
+                iNext = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(iNext);
+
+
+            }
+        });
+
+    }
+}
